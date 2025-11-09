c34935c0942287ddb79b441795eca2a17b509332
greenriver05 <gabrielbd94@gmail.com>
2025-11-09 12:29:50 -0300

    Detalle Inquilino

--- TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/inquilinos/InquilinosFragment.java
+++ TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/inquilinos/InquilinosFragment.java
@@ -1,5 +1,6 @@
 package com.aprendiendo.tpinmobiliariabd.ui.inquilinos;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -13,26 +14,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.aprendiendo.tpinmobiliariabd.R;
+import com.aprendiendo.tpinmobiliariabd.databinding.FragmentInquilinosBinding;
+import com.aprendiendo.tpinmobiliariabd.modelos.Inmueble;
+import com.aprendiendo.tpinmobiliariabd.modelos.Inquilino;
+
+import java.util.List;
 
 public class InquilinosFragment extends Fragment {
 
-    private InquilinosViewModel mViewModel;
+    private InquilinosViewModel mv;
+    private FragmentInquilinosBinding binding;
 
-    public static InquilinosFragment newInstance() {
-        return new InquilinosFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inquilinos, container, false);
+
+        mv = new ViewModelProvider(this).get(InquilinosViewModel.class);
+        binding = FragmentInquilinosBinding.inflate(inflater, container, false);
+
+        mv.getmInquilino().observe(getViewLifecycleOwner(), new Observer<Inquilino>() {
+            @Override
+            public void onChanged(Inquilino inquilino) {
+                binding.tvCodigo.setText(inquilino.getIdInquilino() + "");
+                binding.tvNombre.setText(inquilino.getNombre() + "");
+                binding.tvApellido.setText(inquilino.getApellido() + "");
+                binding.tvDni.setText(inquilino.getDni());
+                binding.tvTelefono.setText(inquilino.getTelefono() + "");
+                binding.tvEmail.setText(inquilino.getEmail() + "");
+            }
+        });
+
+        mv.recuperarInquilino(getArguments());
+        return binding.getRoot();
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(InquilinosViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
--- TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/inquilinos/InquilinosViewModel.java
+++ TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/inquilinos/InquilinosViewModel.java
@@ -1,7 +1,33 @@
 package com.aprendiendo.tpinmobiliariabd.ui.inquilinos;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import com.aprendiendo.tpinmobiliariabd.modelos.Inquilino;
 
-public class InquilinosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+public class InquilinosViewModel extends AndroidViewModel {
+
+    private MutableLiveData<Inquilino> mInquilino = new MutableLiveData<>();
+
+    public InquilinosViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<Inquilino> getmInquilino() { return mInquilino; }
+
+
+    public void recuperarInquilino(Bundle bundle) {
+        Inquilino inquilino = (Inquilino) bundle.get("inquilino");
+        if (inquilino == null) {
+            Toast.makeText(getApplication(), "Error al recuperar el Inquilino", Toast.LENGTH_LONG).show();
+        } else {
+            mInquilino.setValue(inquilino);
+        }
+    }
+
 }
