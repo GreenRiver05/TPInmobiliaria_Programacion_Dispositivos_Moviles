d3474829d6103a1eefb0bd310121608714f7b8f6
greenriver05 <gabrielbd94@gmail.com>
2025-11-09 21:03:38 -0300

    Configuracion de Permiso / Sensor  y Dialogos

--- TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/login/LoginActivity.java
+++ TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/login/LoginActivity.java
@@ -1,9 +1,20 @@
 package com.aprendiendo.tpinmobiliariabd.ui.login;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -12,6 +23,7 @@ import com.aprendiendo.tpinmobiliariabd.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CALL_PERMISSION = 100;
     private LoginActivityViewModel mv;
     private ActivityLoginBinding binding;
 
@@ -44,7 +56,93 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        validarPermisoLlamada();
+        mv.registrarSensor();
+    }
 
 
+    //----------------------------------------Uso de permiso, Referencia: Foro en aula virtual----------------------------------------------------
+    //implementaciones de Dialo mirando pildora Informativa 6 que se encuentra en el Aula ademas del foro del aula donde se explica permiso
+    private void validarPermisoLlamada() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
+        //En versiones < Android 6 no se requiere pedir permisos en tiempo de ejecución.
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) { // Si el permiso de llamada no está otorgado.
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+                mostrarDialogoExplicativo(); //Si el usuario rechazó antes, explico por qué lo necesito.
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PERMISSION);
+                //Solicito el permiso directamente.
+            }
+        }
+    }
+
+    private void mostrarDialogoExplicativo() {
+        new AlertDialog.Builder(this)
+                .setTitle("Permiso necesario")
+                .setMessage("La app necesita permiso para realizar llamadas automáticas en caso de emergencia.")
+                .setPositiveButton("Aceptar", (dialog, which) ->
+                        ActivityCompat.requestPermissions(
+                                this,
+                                new String[]{Manifest.permission.CALL_PHONE},
+                                REQUEST_CALL_PERMISSION))
+                .setNegativeButton("Cancelar", null)
+                .show();
+        // Si el usuario acepta el diálogo explicativo, solicito el permiso CALL_PHONE con el código REQUEST_CALL_PERMISSION.
+        // Esto lanza el diálogo oficial de Android para que el usuario lo acepte.
+        // La respuesta se maneja en onRequestPermissionsResult(...).
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // requestCode: número que identifica qué permiso se está respondiendo (ej. REQUEST_CALL_PERMISSION).
+        // permissions: lista de permisos que se solicitaron.
+        // grantResults: resultados (aceptado o rechazado) para cada permiso solicitado.
+
+
+        if (requestCode == REQUEST_CALL_PERMISSION) { // Verifico que el permiso sea el que me interesa
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //grantResults[0] == PackageManager.PERMISSION_GRANTED: significa que el primer permiso fue aceptado.
+
+                Toast.makeText(this, "Permiso de llamada otorgado", Toast.LENGTH_SHORT).show();
+
+            } else {
+                mostrarDialogoConfiguracionManual(); //Muestro el dialogo de configuración manual
+            }
+        }
+    }
+
+    private void mostrarDialogoConfiguracionManual() {
+        // Este diálogo se muestra si el usuario rechazó el permiso y marcó "No volver a preguntar".
+        // Le doy la opción de ir manualmente a Configuración > Aplicaciones para habilitar el permiso CALL_PHONE.
+
+
+        new AlertDialog.Builder(this)
+                .setTitle("Permiso denegado")
+                .setMessage("Podés habilitar el permiso manualmente desde Configuración > Aplicaciones.")
+                .setPositiveButton("Ir a configuración", (dialog, which) -> {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                    //Si el usuario acepta, ejecuto un intent que lo lleva directamente a la pantalla de configuración de esta app.
+                    //Construyo una URI con el nombre del paquete de esta app para que Android sepa qué configuración abrir.
+                    //Asigno la URI al intent para que apunte a esta app en particular.
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        mv.desregistrarSensor();
     }
 }
--- TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/login/LoginActivityViewModel.java
+++ TPInmobiliariaBD2/app/src/main/java/com/aprendiendo/tpinmobiliariabd/ui/login/LoginActivityViewModel.java
@@ -1,12 +1,20 @@
 package com.aprendiendo.tpinmobiliariabd.ui.login;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.Manifest;
 import android.app.Application;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
@@ -22,15 +30,67 @@ import retrofit2.Response;
 public class LoginActivityViewModel extends AndroidViewModel {
 
     private MutableLiveData<String> mMensaje = new MutableLiveData<>();
-private SensorManager manager;
-private List<Sensor> sensores;
+    private SensorManager manager;
+    private List<Sensor> sensores;
+    private ManejaEventos maneja;
 
-    public LoginActivityViewModel(@NonNull Application application) { super(application); }
+    public LoginActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public MutableLiveData<String> getmMensaje() { return mMensaje; }
 
 
-    public MutableLiveData<String> getmMensaje() {return mMensaje;}
+    public void registrarSensor() {
+        manager = (SensorManager) getApplication().getSystemService(SENSOR_SERVICE);  // Obtengo el servicio de sensores del sistema.
+        sensores = manager.getSensorList(Sensor.TYPE_ACCELEROMETER); //Uso acelerómetro porque para detectar agitación.
+        if (sensores.size() > 0) {
+            maneja = new ManejaEventos(); // Instancio listener personalizado para manejar eventos del sensor
+            manager.registerListener(maneja, sensores.get(0), SensorManager.SENSOR_DELAY_NORMAL); //Registro el listener con frecuencia alta.
+        }
+    }
+
+    public void desregistrarSensor() {
+        manager.unregisterListener(maneja);
+    }
+
+    private void realizarLlamada() { //Clase 29 de agosto 2025
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse("tel:2657000000"));
+        getApplication().startActivity(intent);
+    }
+
+    private class ManejaEventos implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            // Calcular la magnitud de la aceleración usando la fórmula Math.sqrt(x² + y² + z²).
+            float aceleracion = (float) Math.sqrt(x * x + y * y + z * z);
+
+            // Verificar si la magnitud de la aceleración supera un umbral
+            //En reposo, el teléfono tiene ~9.8 m/s² por la gravedad.
+            if (aceleracion > 20) {
 
 
+                // Verificar si el permiso de llamada está otorgado.
+                // Un poco visto en clase 29 de agosto 2025 otro poco en el foro del aula
+                if (ContextCompat.checkSelfPermission(getApplication(), Manifest.permission.CALL_PHONE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    realizarLlamada();
+                }
+
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    }
 
     public void logueo(String usuario, String clave) {
         if (usuario.isEmpty() || clave.isEmpty()) {
